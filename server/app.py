6336a456512d32e0929d04f6b3f144e078554bed
Ildar Baimuratov <baimuratov.i@gmail.com>
2025-08-27 21:33:15 +0200

    flask -> fastapi + openapi

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 import os
 import json
-from flask import *
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import uvicorn
 import rdflib
 from owlready2 import *
 import sys
@@ -17,24 +19,23 @@ rule_base_path = os.path.join(dir_path, rule_base)
 with open(rule_base_path) as file:
     rules = json.load(file)
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/api/rule', methods=['GET'])
-def get_rules():
-    return jsonify(list(rules.keys())), 200
+@app.get('/api/rule')
+async def get_rules():
+    return list(rules.keys())
 
-@app.route('/api/rule/<id>', methods=['GET'])
-def get_rule(id):
-    file = os.path.join(rule_files, rules[id]['file'])
-    return send_file(file), 200
+@app.get('/api/rule/{id}')
+async def get_rule(id: str):
+    file = os.path.join(dir_path, rule_files, rules[id]['file'])
+    return FileResponse(file)
 
-@app.route('/api/rule/<id>', methods=['POST'])
-def check_model(id):
+@app.post('/api/rule/{id}')
+async def check_model(id: str, file: UploadFile=File(...)):
     
-    files = request.files
-    file = files[next(iter(files))]
     file_path_ttl = os.path.join(dir_path, temp_data_ttl)
-    file.save(file_path_ttl)
+    with open(file_path_ttl, 'wb') as file_saved:
+        file_saved.write(file.file.read())
 
     g = rdflib.Graph()
     g.parse(file_path_ttl)
@@ -62,7 +63,7 @@ def check_model(id):
             'valid': True
         }
         onto.destroy(update_relation=True, update_is_a=True)
-        return jsonify(res), 200
+        return res
     except Exception as e:
         if type(e).__name__ == 'OwlReadyInconsistentOntologyError':
             res = {
@@ -70,12 +71,11 @@ def check_model(id):
                 'log': str(e)
             }
             onto.destroy(update_relation=True, update_is_a=True)
-            return jsonify(res), 200
+            return res
         else:
             onto.destroy(update_relation=True, update_is_a=True)
-            return jsonify(str(e)), 400
-    
+            return e
 
 
 if __name__ == "__main__":
-    app.run()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
