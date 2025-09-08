6996cd8c1562bcbfb9b5fa99f59ca2e58c4ecc87
Ildar Baimuratov <baimuratov.i@gmail.com>
2025-09-08 12:46:17 +0200

    + main.py

--- server/app.py
+++ server/app.py
@@ -1,80 +1,22 @@
-import os
-import json
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
 import uvicorn
-import rdflib
-from owlready2 import *
-import sys
+from main import *
 
-
-dir_path = os.path.dirname(os.path.abspath(__file__))
-rule_files = 'rules/'
-rule_base = 'rules.json'
-temp_data_ttl = 'temp_data.ttl'
-temp_data_nt = 'temp_data.nt'
-log_file = 'temp_log.txt'
-
-rule_base_path = os.path.join(dir_path, rule_base)
-with open(rule_base_path) as file:
-    rules = json.load(file)
 
 app = FastAPI()
 
 @app.get('/api/rule')
-async def get_rules():
-    return list(rules.keys())
+async def api_get_rules():
+    return get_rules()
 
 @app.get('/api/rule/{id}')
-async def get_rule(id: str):
-    file = os.path.join(dir_path, rule_files, rules[id]['file'])
-    return FileResponse(file)
+async def api_get_rule(id: str):
+    return FileResponse(get_rule(id))
 
 @app.post('/api/rule/{id}')
-async def check_model(id: str, file: UploadFile=File(...)):
-    
-    file_path_ttl = os.path.join(dir_path, temp_data_ttl)
-    with open(file_path_ttl, 'wb') as file_saved:
-        file_saved.write(file.file.read())
-
-    g = rdflib.Graph()
-    g.parse(file_path_ttl)
-    file_path_nt = os.path.join(dir_path, temp_data_nt)
-    g.serialize(destination=file_path_nt, format='nt')
-
-    #onto_path.append(file_path_nt)
-    onto = get_ontology(os.path.join('file://', file_path_nt)).load()
-    for inst in onto.individuals():
-        close_world(inst)
-    onto.save(file_path_nt)
-    
-    try:
-        log_file_path = os.path.join(dir_path, log_file)
-        sys.stdout = open(log_file_path, 'w')
-        with onto:
-                sync_reasoner_pellet(
-                infer_property_values = True,
-                infer_data_property_values = True,
-                debug=2)
-        sys.stdout.close()
-        with open(log_file_path) as file:
-            log = file.read()
-        res = {
-            'valid': True
-        }
-        onto.destroy(update_relation=True, update_is_a=True)
-        return res
-    except Exception as e:
-        if type(e).__name__ == 'OwlReadyInconsistentOntologyError':
-            res = {
-                'valid': False,
-                'log': str(e)
-            }
-            onto.destroy(update_relation=True, update_is_a=True)
-            return res
-        else:
-            onto.destroy(update_relation=True, update_is_a=True)
-            return e
+async def api_check_model(id: str, file: UploadFile=File(...)):
+    return check_model(id, file)
 
 
 if __name__ == "__main__":
---
+++ server/main.py
@@ -0,0 +1,69 @@
+import os
+import rdflib
+from owlready2 import *
+import json
+import sys
+
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+rule_files = 'rules/'
+rule_base = 'rules.json'
+temp_data_ttl = 'temp_data.ttl'
+temp_data_nt = 'temp_data.nt'
+log_file = 'temp_log.txt'
+
+rule_base_path = os.path.join(dir_path, rule_base)
+with open(rule_base_path) as file:
+    rules = json.load(file)
+
+
+def get_rules():
+    return list(rules.keys())
+
+def get_rule(id: str):
+    return os.path.join(dir_path, rule_files, rules[id]['file'])
+    
+def check_model(id: str, file):
+    
+    file_path_ttl = os.path.join(dir_path, temp_data_ttl)
+    with open(file_path_ttl, 'wb') as file_saved:
+        file_saved.write(file.file.read())
+
+    g = rdflib.Graph()
+    g.parse(file_path_ttl)
+    file_path_nt = os.path.join(dir_path, temp_data_nt)
+    g.serialize(destination=file_path_nt, format='nt')
+
+    #onto_path.append(file_path_nt)
+    onto = get_ontology(os.path.join('file://', file_path_nt)).load()
+    for inst in onto.individuals():
+        close_world(inst)
+    onto.save(file_path_nt)
+    
+    try:
+        log_file_path = os.path.join(dir_path, log_file)
+        sys.stdout = open(log_file_path, 'w')
+        with onto:
+                sync_reasoner_pellet(
+                infer_property_values = True,
+                infer_data_property_values = True,
+                debug=2)
+        sys.stdout.close()
+        with open(log_file_path) as file:
+            log = file.read()
+        res = {
+            'valid': True
+        }
+        onto.destroy(update_relation=True, update_is_a=True)
+        return res
+    except Exception as e:
+        if type(e).__name__ == 'OwlReadyInconsistentOntologyError':
+            res = {
+                'valid': False,
+                'log': str(e)
+            }
+            onto.destroy(update_relation=True, update_is_a=True)
+            return res
+        else:
+            onto.destroy(update_relation=True, update_is_a=True)
+            return str(e)
